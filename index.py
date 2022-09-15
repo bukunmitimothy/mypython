d5caaeb2e1e08b0bb9476a916f6e07433cc5a6e4
bukunmitimothy <102929055+bukunmitimothy@users.noreply.github.com>
2022-09-15 16:48:25 +0100

    index.py
    this is python

---
+++ index.py
@@ -0,0 +1,12 @@
+# mypython to just question
+your_name = input("what is your name? ")
+place = input("where are you now? ")
+if your_name >=4 and place <= 250 :
+  print("welcome " + " so nice of you to join us again")
+  
+elif your_name <= 3 and place  > 250:
+    print("be change your location")
+    
+else:
+    print("not avabile for you baba")
+#new task
